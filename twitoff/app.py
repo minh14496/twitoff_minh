41b5a0591f2ba82f82392729339bc3bd63587cf6
minh14496 <minh14496@gmail.com>
2021-05-17 11:00:16 -0700

    update app.py

--- twitoff/app.py
+++ twitoff/app.py
@@ -3,13 +3,41 @@
 """
 
 from flask import Flask, render_template
+from .models import DB, User
 
 
 def create_app():
+    """
+    create_app Creates and configures and instance of the flask application
+    """
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db,.sqlite3"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    DB.init_app(app)
 
     @app.route('/')
     def root():
-        return render_template("base.html", title="Home")
+        users = User.query.all()
+        return render_template("base.html", title="Home", users=users)
+
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        users = User.query.all()
+        return render_template("base.html", title="Home", users=users)
+
+    @app.route('/populate')
+    def populate():
+        insert_users(["elonmusk", "jackblack"])
+        users = User.query.all()
+        return render_template("base.html", title="Home", users=users)
 
     return app
+
+def insert_users(usernames):
+    for id_indexx, username in enumerate(usernames):
+        user = User(id=id_indexx, username=username)
+        DB.session.add(user)
+        DB.session.commit()
